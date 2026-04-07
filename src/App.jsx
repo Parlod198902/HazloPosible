80d106287d377904d3eb15a97874842d2a2e39f6
Parlod198902 <ivanalxeigamboabernal@hotmail.com>
2026-04-07 14:23:18 -0600

    Subiendo mi proyecto Hazlo Posible

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import heroImg from './assets/HERO.png'
+import logoImg from './assets/HAZLO POSIBLE LOGO.png'
+import './index.css'
+
+function App() {
+  return (
+
+      /* Sección Hero */
+    <div className="hero-section">
+      {/* Imagen de fondo */}
+      <img src={heroImg} alt="Hazlo Posible" className="hero-image" />
+      
+      {/* Contenido superpuesto */}
+      <div className="hero-content">
+        <div className="hero-text">
+          <h1 className="hero-title"> <img src={logoImg} alt="Hazlo Posible" /></h1>
+          <h2 className="hero-subtitle">Cada acción cuenta</h2>
+          <p className="hero-description">Únete como donador o voluntario en menos de 1 minuto</p>
+          
+          {/* Botones */}
+          <div className="hero-buttons">
+            <button className="btn btn-donate"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>Donar</button>
+            <button className="btn btn-volunteer"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 7a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg> Ser voluntario</button>
+          </div>
+          
+          <p className="hero-stats">Más de 2,500 personas ya se han unido</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
